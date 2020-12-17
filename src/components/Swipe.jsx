9a16c0736d427cdbdefc6e2bec6449d4831a4025
jbrown7407 <jbrown7407@gmail.com>
2020-12-16 19:10:27 -0500

    shuffle with SWIPE button

--- src/components/Swipe.jsx
+++ src/components/Swipe.jsx
@@ -1,69 +1,85 @@
 import React, { Component } from "react";
 // import Meal from "./Meal"
 
-export default class Swipe extends Component {
+import Button from "react-bootstrap/Button";
 
+export default class Swipe extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      meals: this.props.meals
+      meals: this.props.meals, 
+      currentMeal: {}
     };
+    this.clicked2=this.clicked2.bind(this)
+  }
+  // console.log(this.state.meals)
+    
+  //when page loads, meals as an array
+  mealArray = Object.values(this.props.meals) 
+  //  mealArray.push(Object.values(this.state.meals))
 
+  // x = 0
+  // y = 0
+  // meal1 = Object.values(this.props.meals)[x]
+  // // console.log(typeof meal1)
+  // meal2 = Object.values(this.props.meals)[y]
+  // console.log(typeof mealArray)
+
+   pullNewIndex = () => {
+    let x = Math.ceil(Math.random() * (this.state.meals.length - 1))
+    let y = Math.ceil(Math.random() * (this.state.meals.length - 1))
+   }
+  initialMeal = () => {
+    this.setState({currentMeal: this.props.meals[0]})
+  }
+  
+
+   clicked2 = () => {
+    console.log("button was clicked")
+    let x = Math.ceil(Math.random() * (this.state.meals.length - 1))
+    // console.log(x)
+    // console.log(this.state.meals[x])
+    let meal1 = this.state.meals[x]
+    console.log(meal1)
+     this.setState({ currentMeal: meal1 })
+     console.log(this.state.currentMeal.pic)
+   }
+  componentDidMount() {
+    this.initialMeal()
+  }
+  // leftClick = () => {
+  //   console.log("left click. add to dislike array for user")
+  //   }
+  // rightClick = () => {
+  //       console.log("right click. add to favs array. option to link to restaurant")
+  //   }
+  
+  changeMeal = () => {
+    console.log("change meal")
   }
 
+
+ ///// ERROR COMING FROM FUNCTION CALL
+  // this.pullNewIndex()
+  
   render() {
-
-    console.log(this.state.meals)
-    
-    //when page loads, meals as an array
-    let mealArray = Object.values(this.props.meals)
-    //  mealArray.push(Object.values(this.state.meals))
-    // let x = this.state.index
-    let x = 0
-    let y = 0
-
-    function pullNewIndex() {
-      x = Math.ceil(Math.random() * (mealArray.length-1))
-      y = Math.ceil(Math.random() * (mealArray.length-1))
-    }
-    pullNewIndex()
-    let meal1 = mealArray[x]
-    console.log(typeof meal1)
-    const meal2 = mealArray[y]
-    console.log(typeof mealArray)
-    //state tracks current
-    function buttonClicked() {
-      function handleClick(e) {
-        e.preventDefault();
-        console.log('The link was clicked.');
-        x = x + 1
-        console.log(x)
-       meal1 = mealArray[x]
-      }
-    }
-    // leftClick() {
-    //   console.log("left click. add to dislike array for user")
-    //     }
-    //   rightClick() {
-    //       console.log("right click. add to favs array. option to link to restaurant")
-    //     }
     return (
       <article className="card">
-        {/* loop through items. SHOW JUST FIRST ITEM
-         if this.handleclick left => dotn want (restart loop and i++)
-        else this.handleclick right => do this isntead (push to favorites array and then i++) */}
-          
       
-        {/* <h1> {this.state.index} </h1> */}
-         <img src={meal1.pic} /> 
-        <h1> <a href={meal1.restlink}> Link to Order</a> </h1>
-        <h1> ID: {meal1.id} </h1>
-        <h1> Food: {meal1.meal} </h1>
+        {/* <img src={this.state.meals[this.state.currentMeal]} /> */}
+        {console.log(this.state.currentMeal)}
+        {/* set to value x */}
 
- <button onClick={buttonClicked()}> UP  </button>
-        <button> Dislike </button>
-        <button> Favorite </button>
-       
+        <h1> {this.state.index} </h1> 
+           <img src={this.state.currentMeal.pic} />  
+        <h1> <a href={this.state.currentMeal.restlink}> Link to Order</a> </h1>
+        <h1> ID: {this.state.currentMeal.id} </h1>
+        <h1> Food: {this.state.currentMeal.meal} </h1> 
+        <Button onClick={() => this.clicked2()} variant="primary">SWIPE</Button>
+
+        {/* <Button onClick={() => leftClick()} variant="primary">DISLIKE</Button>
+        <Button onClick={() => rightClick()} variant="primary">LIKE</Button> */}
+ 
       </article>
     );
   }
